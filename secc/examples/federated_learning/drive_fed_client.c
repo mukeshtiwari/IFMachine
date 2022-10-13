a91f50af8fda8fe102eda88e58a21e995c67798b
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2022-10-13 15:21:11 +0100

    Changes and disregarded differential privacy to compute m and c

--- secc/examples/federated_learning/drive_fed_client.c
+++ secc/examples/federated_learning/drive_fed_client.c
@@ -23,8 +23,8 @@ to the server.
 
 struct private_training_data
 {
-  double x;
-  double y;
+  float x;
+  float y;
   struct private_training_data *next;
 };
 
@@ -48,79 +48,78 @@ typedef enum ret_code status_code;
 struct event
 {
   event_status est;
-  double m; 
-  double c; 
+  float m; 
+  float c; 
   // When est == Training
-  double m_old; // old m 
-  double c_old; // old c 
-  double dm; // averaged m 
-  double dc; // averaged c
-  double noise1; // noise added to m_new = m_old - learning_rate * (dm + noise1)
-  double noise2; // noise added to c_new = c_old - learning_rate * (dc + noise2)
+  float m_old; // old m 
+  float c_old; // old c 
+  float dm; // averaged m 
+  float dc; // averaged c
+  float noise1; // noise added to m_new = m_old - learning_rate * (dm + noise1)
+  float noise2; // noise added to c_new = c_old - learning_rate * (dc + noise2)
 };
 
 struct yvector
 {
-  double head;
+  float head;
   struct yvector *next;
 };
 
 struct gradient
 {
-  double m;
-  double c;
+  float m;
+  float c;
   struct gradient *next;
 };
 
-double *budget;
-double eps; 
-double refvalue;
+float *budget;
+float eps; 
+float refvalue;
 struct  private_training_data *xs;
 struct yvector *ys;
 struct gradient *gs;
 int n;
-double *m;
-double *c;
-double *mret; 
-double *cret;
-double *learning_rate;
+float *m;
+float *c;
+float *mret; 
+float *cret;
+float *learning_rate;
 int k;
 
-extern void compute_yhat_from_data(double m, double c, struct private_training_data *xs, struct yvector *ys, int n);
+extern void compute_yhat_from_data(float m, float c, struct private_training_data *xs, struct yvector *ys, int n);
 extern void compute_gradient_m_and_c(struct yvector *yhat, struct private_training_data *xs, struct gradient *gs, int n);
 extern int abs(int x);
-extern void clip_gradient(struct gradient *gs, int n, double tau);
-extern double sum_gradient_m(struct gradient *gs, int n);
-extern double average_gradient_m(struct gradient *gs, int n);
-extern double sum_gradient_c(struct gradient *gs, int n);
-extern double average_gradient_c(struct gradient *gs, int n);
-extern status_code start_the_client(double *budget, double eps, double refvalue);
-extern status_code fill_budget(double *budget, double eps, double refvalue);
-extern void recursive_gradient_descent(int k, struct  private_training_data *xs, int n, double *m, double *c, double *learning_rate, 
-  double *budget, double eps, double refvalue, struct yvector *yhat, struct gradient *gs);
-extern status_code predict_m_and_c_using_grad_descent(struct  private_training_data *xs, int n, double *m, double *c,
-  double *mret, double *cret, double *learning_rate, double *budget, double eps, double refvalue, int k, struct yvector *yhat, struct gradient *gs);
+extern void clip_gradient(struct gradient *gs, int n, float tau);
+extern float sum_gradient_m(struct gradient *gs, int n);
+extern float average_gradient_m(struct gradient *gs, int n);
+extern float sum_gradient_c(struct gradient *gs, int n);
+extern float average_gradient_c(struct gradient *gs, int n);
+extern status_code start_the_client(float *budget, float eps, float refvalue);
+extern status_code fill_budget(float *budget, float eps, float refvalue);
+extern void recursive_gradient_descent(int k, struct  private_training_data *xs, int n, float *m, float *c, float *learning_rate, 
+  float *budget, float eps, float refvalue, struct yvector *yhat, struct gradient *gs);
+extern status_code predict_m_and_c_using_grad_descent(struct  private_training_data *xs, int n, float *m, float *c,
+  float *mret, float *cret, float *learning_rate, float *budget, float eps, float refvalue, int k, struct yvector *yhat, struct gradient *gs);
 extern status_code run_the_learning_framework();
 
 
 
-/* 
-Laplace implementation goes here.
-https://www.boost.org/doc/libs/1_49_0/libs/math/doc/sf_and_dist/html/math_toolkit/dist/dist_ref/dists/laplace_dist.html
-http://www.mymathlib.com/functions/probability/laplace_distribution.html
-In this implementation, mu = 0 /\ sigma = 1.
-f(x, mu, sigma) = 1 / (2 * sigma) * exp(-|x - mu| / sigma)
-f(x,0,1) = 1 / (2 * 1) * exp(-|x| / 1)
-f(x,0,1) = 0.5 * exp(-|x|)
-returns a random number from a Laplace distribution with parameter tau
-To get the actual predication, comment out the noise part
-and just simply return 0, i.e. no noise.
-*/
-double laplace_noise(double x)
+// Laplace implementation goes here.
+// https://www.boost.org/doc/libs/1_49_0/libs/math/doc/sf_and_dist/html/math_toolkit/dist/dist_ref/dists/laplace_dist.html
+// http://www.mymathlib.com/functions/probability/laplace_distribution.html
+// In this implementation, mu = 0 /\ sigma = 1.
+// f(x, mu, sigma) = 1 / (2 * sigma) * exp(-|x - mu| / sigma)
+// f(x,0,1) = 1 / (2 * 1) * exp(-|x| / 1)
+// f(x,0,1) = 0.5 * exp(-|x|)
+// returns a random number from a Laplace distribution with parameter tau
+// To get the actual predication, comment out the noise part
+// and just simply return 0, i.e. no noise.
+float laplace_noise(float x)
 {
-   double temp = 0.5 * exp(-fabs(x));
-   return (x <= 0.0) ? temp : 1.0 - temp;
-   //return 0;
+   float temp = 0.5 * exp(-fabs(x));
+   //printf("x and laplace_noise:%f %f\n", x, temp);
+   //return (x <= 0.0) ? temp : 1.0 - temp;
+   return 0; // disregard any differential privacy (this is for testing because we return no noise)
 }
 
 pthread_mutex_t g_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -133,7 +132,7 @@ void release_lock(){
   pthread_mutex_unlock(&g_mutex);
 }
 
-struct event add_receive_event_to_trace(double m, double c)
+struct event add_receive_event_to_trace(float m, float c)
 {
   FILE *fp = fopen(LOGFILE, "a");
   fprintf(fp, "Received %f %f\n", m, c);
@@ -149,25 +148,9 @@ struct event add_refill_event_to_trace()
   return (struct event) {.est = Refilled};
 }
 
-struct event add_training_event_to_trace(double *m, double *c, double *budget, double dm, 
-  double dc, double eps, double learn_rate)
+struct event add_training_event_to_trace(float m_new, float m_old, float dm,
+  float noise1, float c_new, float c_old, float dc, float noise2, float learning_rate)
 {
-
-  double noise1 = laplace_noise(4 * 1.0/eps)/n;
-  double noise2 = laplace_noise(4 * 1.0/eps)/n;
-
-  double m_old = *m;
-  double c_old = *c;
-  double m_new; 
-  double c_new;
-
-  m_new = m_old - learn_rate * (dm + noise1);
-  c_new = c_old - learn_rate * (dc + noise2);
-
-  *m = m_new;
-  *c = c_new;
-  *budget = *budget - eps; 
-
   FILE *fp = fopen(LOGFILE, "a");
   fprintf(fp, "Training %f %f\n", m, c);
   fclose(fp);
@@ -175,7 +158,7 @@ struct event add_training_event_to_trace(double *m, double *c, double *budget, d
     .dm = dm, .noise1 = noise1, .c_old = c_old, .dc = dc, .noise2 = noise2};
 }
 
-struct event add_conveyed_event_to_trace(double m, double c)
+struct event add_conveyed_event_to_trace(float m, float c)
 {
   FILE *fp = fopen(LOGFILE, "a");
   fprintf(fp, "Conveyed %f %f\n", m, c);
@@ -184,9 +167,9 @@ struct event add_conveyed_event_to_trace(double m, double c)
 }
 
 
-double generate_random_number() // [0, 1]
+float generate_random_number() // [0, 1]
 {
-  return (double)rand() / (double)RAND_MAX;
+  return (float)rand() / (float)RAND_MAX;
 }
 
 
@@ -201,7 +184,7 @@ struct private_training_data *alloc_memory_xs(int n)
   {
     struct private_training_data *temp = (struct private_training_data *)malloc(sizeof(struct private_training_data));
     temp->x = generate_random_number();
-    temp->y = temp->x; //m = 1.0, c = 0.0
+    temp->y = 0.5 * temp->x + 0.5 ; //m = 0.5, c = 0.5 but change this to anything and see the prediction
     temp->next = alloc_memory_xs(n-1);
     return temp;
   }
@@ -233,7 +216,7 @@ struct gradient *alloc_memory_gs(int n)
   }
 }
 
-void print_gradient(status_code t, double m, double c)
+void print_gradient(status_code t, float m, float c)
 {
   printf("Trained Parameters: Status code = %d m = %f c = %f\n", t, m, c);
 }
@@ -243,11 +226,12 @@ void* drive_the_learning_framework()
 {
   status_code t;
   int f;
-  double b;
+  float b;
   do 
   {
     t = run_the_learning_framework();
     b = *budget;
+    // f = (fabs(b) <= 1e-6);
   } while(t);
   
   return 0;
@@ -268,13 +252,13 @@ struct private_training_data *read_csv_file(int n)
 
   struct private_training_data *ts;
   ts = xs;
-  double crime, nox;
+  float crime, nox;
   int i = 0;
   while(i < n)
   {
-    fscanf(fp, "%lf %lf", &crime, &nox);
-    xs->x = crime;
-    xs->y = nox;
+    fscanf(fp, "%f %f", &crime, &nox);
+    xs->x = nox;
+    xs->y = crime;
     xs = xs->next;
     i++;
   }
@@ -282,19 +266,27 @@ struct private_training_data *read_csv_file(int n)
 }
 
 
+/*
 
+Notes: This program works excellent, almost predict the  m = 0.999677 c = 0.000208 for 
+the artificially generated data xs = alloc_memory_xs(n) (with m = 1.0 and c = 0.0)
+if I don't add any noise, i.e., return 0 from laplace_noise. Also, make 
+refvalue = 16,000 to have more iterations and comment the 
+sleep function, in the last function,  in client_fed_learn.c.
+
+*/
 
 
 int main()
 {
   
   // initialize the data structures
-  double bud; // budget
+  float bud; // budget
   budget = &bud; // pointer to budget
-  eps = 0.01; // epsilon
-  refvalue = 16.0; // this is the value of the reference budget
+  eps = 0.1; // epsilon
+  refvalue = 1600000.0; // this is the value of the reference budget
   n = 332; // number of training data points
-  k = 1000; // number of iterations
+  k = 10; // number of iterations
 
   xs = alloc_memory_xs(n); // pointer to the list of training data
   ys = alloc_memory_ys(n); // pointer to the list of yhats
@@ -303,20 +295,19 @@ int main()
   read the file crime_nox.csv and 
   write the values in xs.
   */
-  // comment this line to run the algorithm on 
-  // synthetic data (m = 1.0 and c = 0.0)
-  // xs = read_csv_file(n);
+  xs = alloc_memory_xs(n); //artifically generated data so go the function and change m and c and run this code see the prediction  
 
+  //xs = read_csv_file(n); 
 
-  double tmret; 
-  double cmret;
+  float tmret; 
+  float cmret;
   mret = &tmret;
   cret = &cmret;
-  double tm = 0.5;
-  double tc = 0.5;
+  float tm = 0.5;
+  float tc = 0.5;
   m = &tm; // we start with a value of m = 0.5
   c = &tc; // we start with a value of c = 0.5
-  double lrate = 0.1;
+  float lrate = 0.1;
   learning_rate = &lrate;
 
   // start the client
